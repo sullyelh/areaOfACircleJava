833d99989021da16770e110699d2d35d05dc023e
sully <sully.elhassan@gmail.com>
2022-04-29 18:23:31 -0400

    Added program code

--- areaOfACircleJava/src/areaOfACircleJava/areaProgram.java
+++ areaOfACircleJava/src/areaOfACircleJava/areaProgram.java
@@ -1,9 +1,18 @@
 package areaOfACircleJava;
 
+import java.util.Scanner;
+
 public class areaProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Area of circle = (Pi * r)^2 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please enter the radius!");
+		double radius = sc.nextDouble();
+		
+		System.out.println("The area of this circle is " +Math.PI * radius * radius);
 
 	}
 
